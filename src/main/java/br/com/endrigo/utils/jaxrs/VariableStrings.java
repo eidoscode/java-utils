f11db2d877e5e8b9885b2e686305edc625012ea1
Endrigo Antonini <antonini@endrigo.com.br>
2013-02-13 12:50:59 -0200

    Adding the class to help working with JAX-RS

---
+++ src/main/java/br/com/endrigo/utils/jaxrs/VariableStrings.java
@@ -0,0 +1,32 @@
+package br.com.endrigo.utils.jaxrs;
+
+/**
+ * Utility class designed to be used with the JAX-RS API. The purpose of it is
+ * to make it possible to expose an JAX-RS Path that receives an varargs
+ * argument.<br/>
+ * What you need to do is to overload the varargs method an use this class as it
+ * would be the varargs parameter. Then use the
+ * {@link VariableStrings#getSplitPath()} to send the value to the original
+ * method.
+ * 
+ * @author antonini
+ * @since 1.0.1
+ * @version 1.0
+ */
+public class VariableStrings {
+
+	private final String[] splitPath;
+
+	public VariableStrings(String unparsedPath) {
+		splitPath = unparsedPath.split("/");
+	}
+
+	/**
+	 * Returns the String split by the slash ("/").
+	 * @return
+	 */
+	public String[] getSplitPath() {
+		return splitPath;
+	}
+
+}
