27ddb92f6b204fe8766234bf24bb1747596260f2
Endrigo Antonini <antonini@endrigo.com.br>
2013-04-13 17:53:08 -0300

    Improving the Variable String

--- src/test/java/br/com/endrigo/util/jaxrs/VariableStringsTest.java
+++ src/test/java/br/com/endrigo/util/jaxrs/VariableStringsTest.java
@@ -3,6 +3,8 @@ package br.com.endrigo.util.jaxrs;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
+
 import org.junit.Test;
 
 
@@ -17,11 +19,31 @@ public class VariableStringsTest {
 		assertNotNull(paths);
 		assertEquals(2, paths.length);
 	}
+	@Test
+	public void testSplitPaths() {
+		String sPaths = "param1/param2";
+		VariableStrings vars = new VariableStrings(sPaths);
+		List<String> paths = vars.getSplitPaths();
+		
+		assertNotNull(paths);
+		assertEquals(2, paths.size());
+	}
 	
 	@Test(expected=NullPointerException.class)
 	public void testSplitNull() {
 		String sPaths = null;
+		new VariableStrings(sPaths);
+	}
+	
+	@Test
+	public void testSplitBlank() {
+		String sPaths = "";
 		VariableStrings vars = new VariableStrings(sPaths);
+		String[] retVars = vars.getSplitPath();
+		List<String> values = vars.getSplitPaths();
+		
+		assertEquals(1, retVars.length);
+		assertEquals(1, values.size());
 	}
 	
 }
