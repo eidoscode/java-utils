956baef8a52031268955e6f87e90a15d453ca3e1
Endrigo Antonini <eantonini@eidoscode.com>
2014-09-27 16:22:20 -0300

    Added javadoc to methods.

--- src/main/java/com/eidoscode/util/io/CompressUtils.java
+++ src/main/java/com/eidoscode/util/io/CompressUtils.java
@@ -15,168 +15,149 @@ import java.util.zip.ZipOutputStream;
 
 import org.apache.log4j.Logger;
 
+/**
+ * Utility class with functions to work with compacted files.
+ * 
+ * @author antonini
+ * @since 1.1.0
+ * @version 1.0
+ * 
+ */
 public final class CompressUtils {
 
-    private static final Logger LOG = Logger.getLogger(CompressUtils.class);
+  private static final Logger LOG = Logger.getLogger(CompressUtils.class);
 
-    private CompressUtils() {
-    }
+  /**
+   * Hide the constructor.
+   */
+  private CompressUtils() {
+  }
 
-    /**
-     * It extract all files from a ZIP File to destiny path. <br/>
-     * Prefer to use the method
-     *
-     * @param zippedFile
-     *            zipped file.
-     * @param fileDestiny
-     *            path where the files are saved.
-     * @throws IOException
-     *             when an error occurs.
-     */
-    public static void extractZipFile(File zippedFile, File fileDestiny) throws IOException {
-        InputStream input = null;
-        input = new FileInputStream(zippedFile);
-        extractZipFile(input, fileDestiny);
-    }
+  /**
+   * It extract all files from a ZIP File to destiny path. <br/>
+   * Prefer to use the method
+   * 
+   * @param zippedFile
+   *          zipped file.
+   * @param fileDestiny
+   *          path where the files are saved.
+   * @throws IOException
+   *           when an error occurs.
+   */
+  public static void extractZipFile(File zippedFile, File fileDestiny) throws IOException {
+    InputStream input = null;
+    input = new FileInputStream(zippedFile);
+    extractZipFile(input, fileDestiny);
+  }
 
-    /**
-     * It extract all files from a ZIP File to destiny path.
-     *
-     * @param input
-     *            InputStream of the Zip file.
-     * @param destiny
-     *            path where the files are saved.
-     * @throws IOException
-     *             when an error occurs.
-     */
-    public static void extractZipFile(InputStream input, File destiny) throws IOException {
-        ZipEntry entry = null;
-        ZipInputStream zinputStream = null;
+  /**
+   * It extract all files from a ZIP File to destiny path.
+   * 
+   * @param input
+   *          InputStream of the Zip file.
+   * @param destiny
+   *          path where the files are saved.
+   * @throws IOException
+   *           when an error occurs.
+   */
+  public static void extractZipFile(InputStream input, File destiny) throws IOException {
+    ZipEntry entry = null;
+    ZipInputStream zinputStream = null;
 
-        zinputStream = new ZipInputStream(new BufferedInputStream(input));
+    zinputStream = new ZipInputStream(new BufferedInputStream(input));
 
-        while ((entry = zinputStream.getNextEntry()) != null) {
+    while ((entry = zinputStream.getNextEntry()) != null) {
 
-            if (entry.isDirectory()) {
-                String currentEntry = entry.getName();
-                File destFile = new File(destiny, currentEntry);
-                destFile.mkdirs();
+      if (entry.isDirectory()) {
+        String currentEntry = entry.getName();
+        File destFile = new File(destiny, currentEntry);
+        destFile.mkdirs();
 
-            } else {
-                String currentEntry = entry.getName();
+      } else {
+        String currentEntry = entry.getName();
 
-                File destFile = new File(destiny, currentEntry);
-                File destinationParent = destFile.getParentFile();
+        File destFile = new File(destiny, currentEntry);
+        File destinationParent = destFile.getParentFile();
 
-                // create the parent directory structure if needed
-                destinationParent.mkdirs();
+        // create the parent directory structure if needed
+        destinationParent.mkdirs();
 
-                LOG.debug("Extracting: " + entry);
+        LOG.debug("Extracting: " + entry);
 
-                int count;
-                byte data[] = new byte[StreamUtils.BUFFER_BYTES];
-                FileOutputStream fos = new FileOutputStream(destFile);
-                BufferedOutputStream dest = new BufferedOutputStream(fos, StreamUtils.BUFFER_BYTES);
-                while ((count = zinputStream.read(data, 0, StreamUtils.BUFFER_BYTES)) != -1) {
-                    dest.write(data, 0, count);
-                }
-                dest.flush();
-                StreamUtils.closeSilently(dest);
-            }
+        int count;
+        byte data[] = new byte[StreamUtils.BUFFER_BYTES];
+        FileOutputStream fos = new FileOutputStream(destFile);
+        BufferedOutputStream dest = new BufferedOutputStream(fos, StreamUtils.BUFFER_BYTES);
+        while ((count = zinputStream.read(data, 0, StreamUtils.BUFFER_BYTES)) != -1) {
+          dest.write(data, 0, count);
         }
-        StreamUtils.closeSilently(zinputStream);
+        dest.flush();
+        StreamUtils.closeSilently(dest);
+      }
+    }
+    StreamUtils.closeSilently(zinputStream);
+  }
+
+  public static File toZip(final File zipFile, com.eidoscode.util.io.zip.ZipEntry... files) throws IOException {
+    int cont;
+    byte[] data = new byte[StreamUtils.BUFFER_BYTES];
+
+    if (zipFile.exists()) {
+      zipFile.delete();
     }
 
-    public static File toZip(final File zipFile, com.eidoscode.util.io.zip.ZipEntry... files) throws IOException {
-        int cont;
-        byte[] data = new byte[StreamUtils.BUFFER_BYTES];
+    final File tempReadingFile = File.createTempFile("tmp-", "-" + zipFile.getName());
 
-        if (zipFile.exists()) {
-            zipFile.delete();
+    if (tempReadingFile.delete()) {
+      BufferedInputStream source = null;
+      FileInputStream streamSource = null;
+      ZipEntry entry = null;
+
+      final File tempWritingFile = File.createTempFile("tmp-", ".w.dat");
+      tempWritingFile.delete();
+
+      ZipOutputStream append = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(tempWritingFile)));
+
+      if (zipFile.exists()) {
+
+        FileUtils.moveFile(zipFile, tempReadingFile, true);
+
+        ZipFile zip = new ZipFile(tempReadingFile);
+
+        Enumeration<? extends ZipEntry> entries = zip.entries();
+        while (entries.hasMoreElements()) {
+          ZipEntry e = entries.nextElement();
+          LOG.debug("Copy: " + e.getName());
+          append.putNextEntry(e);
+          if (!e.isDirectory()) {
+            StreamUtils.copyStream(zip.getInputStream(e), append, false, false);
+          }
         }
 
-        final File tempReadingFile = File.createTempFile("tmp-", "-" + zipFile.getName());
+        StreamUtils.closeSilently(zip);
+      }
 
-        if (tempReadingFile.delete()) {
-            BufferedInputStream source = null;
-            FileInputStream streamSource = null;
-            ZipEntry entry = null;
+      for (com.eidoscode.util.io.zip.ZipEntry file : files) {
+        LOG.debug(file.getFile());
+        streamSource = new FileInputStream(file.getFile());
+        source = new BufferedInputStream(streamSource, StreamUtils.BUFFER_BYTES);
+        entry = new ZipEntry(file.getRelativePath());
+        LOG.debug("Adding: " + file.getRelativePath());
 
-            final File tempWritingFile = File.createTempFile("tmp-", ".w.dat");
-            tempWritingFile.delete();
+        append.putNextEntry(entry);
 
-            ZipOutputStream append = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(tempWritingFile)));
-
-            if (zipFile.exists()) {
-
-                FileUtils.moveFile(zipFile, tempReadingFile, true);
-
-                ZipFile zip = new ZipFile(tempReadingFile);
-
-                Enumeration<? extends ZipEntry> entries = zip.entries();
-                while (entries.hasMoreElements()) {
-                    ZipEntry e = entries.nextElement();
-                    LOG.debug("Copy: " + e.getName());
-                    append.putNextEntry(e);
-                    if (!e.isDirectory()) {
-                        StreamUtils.copyStream(zip.getInputStream(e), append, false, false);
-                    }
-                }
-
-                StreamUtils.closeSilently(zip);
-            }
-
-            for (com.eidoscode.util.io.zip.ZipEntry file : files) {
-                LOG.debug(file.getFile());
-                streamSource = new FileInputStream(file.getFile());
-                source = new BufferedInputStream(streamSource, StreamUtils.BUFFER_BYTES);
-                entry = new ZipEntry(file.getRelativePath());
-                LOG.debug("Adding: " + file.getRelativePath());
-
-                append.putNextEntry(entry);
-
-                while ((cont = source.read(data, 0, StreamUtils.BUFFER_BYTES)) != -1) {
-                    append.write(data, 0, cont);
-                }
-                StreamUtils.closeSilently(source);
-            }
-
-            StreamUtils.closeSilently(append);
-
-            tempReadingFile.delete();
-            FileUtils.moveFile(tempWritingFile, zipFile, true);
+        while ((cont = source.read(data, 0, StreamUtils.BUFFER_BYTES)) != -1) {
+          append.write(data, 0, cont);
         }
-        return zipFile;
-    }
+        StreamUtils.closeSilently(source);
+      }
 
-    /**
-     * Copy a zip file into a temporary file and unzip into a destiny folder
-     *
-     * @param resourcePathFile
-     *            path for resource inputstream
-     * @param tempFileName
-     *            temporary file name
-     * @param tempDir
-     *            temporary directory
-     * @param destinyDir
-     *            destiny directory
-     * @param logger
-     *            Logger for put log informations
-     * @throws IOException
-     *             if an error occur
-     */
-    public static void copyZipToTempDirAndExtract(String resourcePathFile, String tempFileName, String tempDir, String destinyDir) throws IOException {
-        // TODO
-        // int totalBytes =
-        // StreamUtils.countBytes(StreamUtils.class.getClassLoader().getResourceAsStream(resourcePathFile));
-        // String tempZipFile = StringUtils.buildFilePath(tempDir,
-        // tempFileName);
-        // FileOutputStream fos = new FileOutputStream(tempZipFile);
-        // StreamUtils.copyStream(StreamUtils.class.getClassLoader().getResourceAsStream(resourcePathFile),
-        // fos,
-        // totalBytes);
-        // StreamUtils.closeSilently(fos);
-        // StreamUtils.extractZipRecursively(tempZipFile, destinyDir);
+      StreamUtils.closeSilently(append);
+
+      tempReadingFile.delete();
+      FileUtils.moveFile(tempWritingFile, zipFile, true);
     }
+    return zipFile;
+  }
 
 }
--- src/main/java/com/eidoscode/util/io/zip/ZipEntry.java
+++ src/main/java/com/eidoscode/util/io/zip/ZipEntry.java
@@ -2,36 +2,43 @@ package com.eidoscode.util.io.zip;
 
 import java.io.File;
 
+/**
+ * Internal Zip entry. It refers to a physical file and a logical location that
+ * this file must be placed on the generated zip file.
+ * 
+ * @author antonini
+ * @since 1.1.0
+ * @version 1.0
+ */
 public class ZipEntry {
 
-	private String relativePath;
-	private File file;
+  private String relativePath;
+  private File file;
 
+  public ZipEntry() {
+    super();
+  }
 
-	public ZipEntry() {
-		super();
-	}
+  public ZipEntry(File file, String relativePath) {
+    this();
+    this.relativePath = relativePath;
+    this.file = file;
+  }
 
-	public ZipEntry(File file, String relativePath) {
-		this();
-		this.relativePath = relativePath;
-		this.file = file;
-	}
+  public String getRelativePath() {
+    return relativePath;
+  }
 
-	public String getRelativePath() {
-		return relativePath;
-	}
+  public void setRelativePath(String relativePath) {
+    this.relativePath = relativePath;
+  }
 
-	public void setRelativePath(String relativePath) {
-		this.relativePath = relativePath;
-	}
+  public File getFile() {
+    return file;
+  }
 
-	public File getFile() {
-		return file;
-	}
-
-	public void setFile(File file) {
-		this.file = file;
-	}
+  public void setFile(File file) {
+    this.file = file;
+  }
 
 }
